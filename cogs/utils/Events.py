8b0f5d670adde344de9460bbb3288d9cb0f6fa3f
zeroday0619 <zeroday0619@kakao.com>
2021-01-25 11:46:11 +0900

    ok

--- cogs/utils/Events.py
+++ cogs/utils/Events.py
@@ -2,19 +2,38 @@ import discord
 import time
 import datetime
 from discord.ext import commands
+from discord.ext.commands import Context, Bot
 from app.controller.logger import Logger
 
 
 class Events(commands.Cog):
-    def __init__(self, bot):
+    def __init__(self, bot: Bot) -> None:
         self.bot = bot
         self.logger = Logger.generate_log()
 
-    @commands.Cog.listener()
-    async def on_command_error(self, ctx, error):
-        if isinstance(error, commands.CheckFailure):
-            CheckFailure = discord.Embed(
-                title="Permission Error",
-                description="You don't have the permissions to do that!",
+    @staticmethod
+    async def error_binder(ctx: Context, error, exception, title: str, description: str):
+        error(error)
+        if isinstance(error, exception):
+            err = discord.Embed(
+                title=title,
+                description=description,
             )
-            await ctx.send(embed=CheckFailure, delete_after=5)
+            return await ctx.send(embed=err, delete_after=5)
+
+    @commands.Cog.listener()
+    async def on_command_error(self, ctx: Context, error):
+        await self.error_binder(
+            ctx=ctx,
+            error=error,
+            exception=commands.ExtensionError,
+            title="Extension Error",
+            description=error
+        )
+        await self.error_binder(
+            ctx=ctx,
+            error=error,
+            exception=commands.BotMissingPermissions,
+            title="Bot Missing Permissions",
+            description=error
+        )
