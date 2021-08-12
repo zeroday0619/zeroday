eff0e0aef9d093edde8aef4cf69ae52c1569dc8c
Euiseo Cha <zeroday0619_dev@outlook.com>
2021-08-12 17:37:33 +0900

    feat: 부적절한 단어 필터 수정 및 TTS 관련코드 수정

--- extensions/tts/__init__.py
+++ extensions/tts/__init__.py
@@ -64,11 +64,9 @@ class TextToSpeech(commands.Cog):
     @commands.command(name="tts", aliases=["t", "-", "=", "#", "%", "*", "`"])
     @commands.check(blockJam_mini)
     async def talk(self, ctx: Context, *, text: str):
-        try:
-            await self.join(ctx.author)
-            await self._text_to_speech(f"{ctx.author.display_name}님이 말합니다. "+text)
-        except Exception as e:
-            raise RuntimeError(e)
+        await self.join(ctx.author)
+        await self._text_to_speech(f"{ctx.author.display_name}님이 말합니다. "+text)
+
 
     @commands.command("disconnect")
     @commands.check(blockJam_mini)
