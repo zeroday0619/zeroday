ca8ad81c211f12337ace272aa2db941d158a04ae
Euiseo Cha <zeroday0619_dev@outlook.com>
2021-12-14 08:36:12 +0900

    feat: fix zeroday

--- extensions/tts/__init__.py
+++ extensions/tts/__init__.py
@@ -1,13 +1,80 @@
 import discord
 from discord.ext import commands
-from discord import FFmpegPCMAudio, PCMVolumeTransformer, VoiceProtocol, Member
+from discord import PCMVolumeTransformer, VoiceProtocol, Member
 from discord.ext.commands import Context
 from discord.ext.commands import Bot
+
+from io import BytesIO
+from shlex import split
+from subprocess import PIPE, Popen
+from discord.opus import Encoder
+
 from extensions.tts._kakao import KakaoOpenAPI
 from app.controller.logger import Logger
 from io import BytesIO
 from tempfile import TemporaryFile
 from typing import Optional
+
+
+class FFmpegPCMAudio(discord.AudioSource):
+    """Reimplementation of discord.FFmpegPCMAudio with source: bytes support
+    Original Source: https://github.com/Rapptz/discord.py/issues/5192"""
+
+    def __init__(
+        self,
+        source,
+        *,
+        executable="ffmpeg",
+        pipe=False,
+        stderr=None,
+        before_options=None,
+        options=None
+    ):
+        args = [executable]
+        if isinstance(before_options, str):
+            args.extend(split(before_options))
+
+        args.append("-i")
+        args.append("-" if pipe else source)
+        args.extend(("-f", "s16le", "-ar", "48000", "-ac", "2", "-loglevel", "warning"))
+
+        if isinstance(options, str):
+            args.extend(split(options))
+
+        args.append("pipe:1")
+
+        self._stdout = None
+        self._process = None
+        self._stderr = stderr
+        self._process_args = args
+        self._stdin = source if pipe else None
+
+    def _create_process(self) -> BytesIO:
+        stdin, stderr, args = self._stdin, self._stderr, self._process_args
+        self._process = Popen(args, stdin=PIPE, stdout=PIPE, stderr=stderr)
+        return BytesIO(self._process.communicate(input=stdin)[0])
+
+    def read(self) -> bytes:
+        if self._stdout is None:
+            # This function runs in a voice thread, so we can afford to block
+            # it and make the process now instead of in the main thread
+            self._stdout = self._create_process()
+
+        ret = self._stdout.read(Encoder.FRAME_SIZE)
+        return ret if len(ret) == Encoder.FRAME_SIZE else b""
+
+    def cleanup(self):
+        process = self._process
+        if process is None:
+            return
+
+        process.kill()
+        if process.poll() is None:
+            process.communicate()
+
+
+        self._process = None
+
 
 
 def blockJam_mini(ctx: Context):
@@ -53,7 +120,7 @@ class TextToSpeech(commands.Cog):
             file.seek(0)
             if not self.voice.is_playing():
                 Logger.generate_log().info("O")
-                self.voice.play(PCMVolumeTransformer(FFmpegPCMAudio(source=file, pipe=True), volume=150), after=file.close())
+                self.voice.play(PCMVolumeTransformer(FFmpegPCMAudio(data, pipe=True, options='-loglevel "quiet"'), volume=150), after=file.close())
             else:
                 file.close()
                 Logger.generate_log().info("X")
